9a79a5550171da11f453b57869cdf5a716d21d68
Brandon Mejia <brandonmejiabusiness@gmail.com>
2025-02-06 19:00:23 -0500

    in class material

---
+++ Data/Shell/my_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello world"
+echo "My curent shell is $SHELL"
+pwd
+date
---
+++ R/Inclass Material.R
@@ -0,0 +1,24 @@
+Jan 29.
+
+print("Hello world!")
+
+
+user_name <- readline( "What is your name?")
+
+
+c(98,99,100,101,102) <- bond_prices
+
+
+bond_prices <- c(98,99,100,101,102)
+
+intrestRate_
+
+February 2 & 5
+
+stock_prices <- c(100, 150)
+
+names(stock_prices)<- "AAPl"
+
+today <- Sys.Date() # returns today's date
+now <- Sys.time() # returns time now
+print(today)
